d6aa724c07aafbccced5c295c3541a9fbad3ce9a
Rolf Niepraschk <Rolf.Niepraschk@gmx.de>
2023-08-09 11:57:44 +0200

    "Adafruit_LED" --> "Adafruit_7Seg"

--- Adafruit_i2c_7seg_LED.cpp
+++ Adafruit_i2c_7seg_LED.cpp
@@ -1,21 +1,3 @@
-//
-//  https://github.com/rolfn/Adafruit_i2c_7seg_LED
-//
-//  Copyright 2015 Rolf Niepraschk
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program. If not, see http://www.gnu.org/licenses/
-
 #include <Adafruit_i2c_7seg_LED.h>
 
 static const uint8_t ssTable[] = {
@@ -35,59 +17,59 @@ static const uint8_t ssTable[] = {
     0x5E, // d
     0x79, // E
     0x71, // F
-    0x00, //<blank>
+    0x00, // <blank>
 };
 
 // Constructor
-Adafruit_LED::Adafruit_LED() {
+Adafruit_7Seg::Adafruit_7Seg() {
 }
 
-void Adafruit_LED::setDigit(uint8_t pos, uint8_t d, boolean decimalPoint) {
+void Adafruit_7Seg::setDigit(uint8_t pos, uint8_t d, boolean decimalPoint) {
   if (d < sizeof(ssTable)) {
-    Adafruit_LED::setSymbol(pos, decimalPoint ? ssTable[d] | 0x80 : ssTable[d]);
+    Adafruit_7Seg::setSymbol(pos, decimalPoint ? ssTable[d] | 0x80 : ssTable[d]);
   }
 } // setDigit
 
-void Adafruit_LED::setDigit(uint8_t pos, uint8_t d) {
-  Adafruit_LED::setDigit(pos, d, false);
+void Adafruit_7Seg::setDigit(uint8_t pos, uint8_t d) {
+  Adafruit_7Seg::setDigit(pos, d, false);
 } // setDigit
 
-void Adafruit_LED::setSymbol(uint8_t pos, uint8_t d) {
+void Adafruit_7Seg::setSymbol(uint8_t pos, uint8_t d) {
   if (pos < sizeof(displayRam)) {
     displayRam[pos] = d;
     sendLed();
   }
 } // setSymbol
 
-void Adafruit_LED::clearPosition(uint8_t pos) {
+void Adafruit_7Seg::clearPosition(uint8_t pos) {
   if (pos < sizeof(displayRam)) {
     displayRam[pos] = 0;
     sendLed();
   }
 } // clearPosition
 
-void Adafruit_LED::clearAllDigits(void) {
+void Adafruit_7Seg::clearAllDigits(void) {
   displayRam[DIGIT_1] = displayRam[DIGIT_2] =
     displayRam[DIGIT_3] = displayRam[DIGIT_4] = 0;
   sendLed();
 } // clearAllDigits
 
-void Adafruit_LED::setPoint(uint8_t d) {
+void Adafruit_7Seg::setPoint(uint8_t d) {
   displayRam[POINTS] |= d;
   sendLed();
 } // setPoint
 
-void Adafruit_LED::togglePoint(uint8_t d) {
+void Adafruit_7Seg::togglePoint(uint8_t d) {
   displayRam[POINTS] ^= d;
   sendLed();
 } // togglePoint
 
-void Adafruit_LED::clearPoint(uint8_t d) {
+void Adafruit_7Seg::clearPoint(uint8_t d) {
   displayRam[POINTS] &= ~d;
   sendLed();
 } // clearPoint
 
-void Adafruit_LED::clearAllPoints(void) {
+void Adafruit_7Seg::clearAllPoints(void) {
   displayRam[POINTS] = 0;
   sendLed();
 }
--- Adafruit_i2c_7seg_LED.h
+++ Adafruit_i2c_7seg_LED.h
@@ -1,25 +1,11 @@
-//
-//  https://github.com/rolfn/Adafruit_i2c_7seg_LED
-//
-//  Copyright 2015 Rolf Niepraschk
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program. If not, see http://www.gnu.org/licenses/
 
 #ifndef ADAFRUIT_I2C_7SEG_LED_H
 #define ADAFRUIT_I2C_7SEG_LED_H
 
-#include <ht16k33.h> // see: https://github.com/lpaseen/ht16k33
+#include <ht16k33.h>
+/*
+https://github.com/lpaseen/ht16k33
+*/
 
 #define DIGIT_1 0
 #define DIGIT_2 2
@@ -34,9 +20,9 @@
 #define MINUS 0x40
 #define BLANK 0x10
 
-class Adafruit_LED : public HT16K33 {
+class Adafruit_7Seg : public HT16K33 {
  public:
-  Adafruit_LED(); // the class itself
+  Adafruit_7Seg(); // the class itself
   void setDigit(uint8_t pos, uint8_t d);
   void setDigit(uint8_t pos, uint8_t d, boolean decimalPoint);
   void setSymbol(uint8_t pos, uint8_t d);
